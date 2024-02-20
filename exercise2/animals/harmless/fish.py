25f1e31c1510e9da81ae1a04ce1b24114406df3c
lisazakrisson <92362402+lisazakrisson@users.noreply.github.com>
2024-02-20 16:54:09 +0100

    Exercises from day 2

---
+++ exercise2/animals/harmless/fish.py
@@ -0,0 +1,10 @@
+# harmless
+class Fish:
+    def __init__(self):
+        ''' Constructor for this class. '''
+        # Create some member fish
+        self.members = ['Tuna', 'Sardine']
+    def printMembers(self):
+        print('Printing members of the Fish class')
+        for member in self.members:
+            print('\t%s ' % member)
